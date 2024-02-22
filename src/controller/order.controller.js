483e08d5eadb31be8af7c5b61f5598ed52f7a947
Narendra Dev <64129728+narendrabuddiga@users.noreply.github.com>
2024-02-22 16:14:46 +0530

    Added cache to order

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -1,5 +1,6 @@
 const orderService = require('../services/order.service');
 const { responseHandler } = require('../helper/helper');
+const redisClient = require('../db/redis/redis');
 
 const createOrder = async (req, res) => {
     let response = await orderService.createOrder(req.body, req.user);
@@ -11,20 +12,37 @@ const createOrder = async (req, res) => {
 }
 
 const getUserOrderList = async (req, res) => {
-    let response = await orderService.getUserOrderList(req.user);
+    let response;
+    let key = `${req.user.id}_order_list`;
+    const cacheResults = await redisClient.getValue(key);
+    if (cacheResults) {
+        response = JSON.parse(cacheResults);
+    } else {
+        let data = await orderService.getUserOrderList(req.user);
+        redisClient.setValue(key, JSON.stringify(data));
+        response = data;
+    }
     res.status(200).send(response);
 }
 
 const getUserOrderById = async (req, res) => {
-    let response = await orderService.getUserOrderById(req.params.id,req.user);
+    let key = `${req.user.id}_order_${req.params.id}`;
+    const cacheResults = await redisClient.getValue(key);
+    if (cacheResults) {
+        response = JSON.parse(cacheResults);
+    } else {
+        let data = await orderService.getUserOrderById(req.params.id, req.user);
+        redisClient.setValue(key, JSON.stringify(data));
+        response = data;
+    }
     res.status(200).send(response);
 }
 
 const removeUserOrderById = async (req, res) => {
-    let response = await orderService.removeOrderById(req.params.id,req.user);
+    let response = await orderService.removeOrderById(req.params.id, req.user);
     res.status(200).send(response);
 }
 
 module.exports = {
-    createOrder, getUserOrderList, getUserOrderById,removeUserOrderById
+    createOrder, getUserOrderList, getUserOrderById, removeUserOrderById
 }
